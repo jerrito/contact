5a4597606e2b27261374175de8075b3103a2e008
ShaQ Express <shaqexpress@ShaQ-mac-Papa-Kay.local>
2023-12-19 18:00:55 +0000

    clean

--- lib/src/authentication/presentation/bloc/authentication_bloc.dart
+++ lib/src/authentication/presentation/bloc/authentication_bloc.dart
@@ -61,6 +61,8 @@ class AuthenticationBloc
         ),
       );
     });
+
+    //!Code Sent
     on<CodeSentEvent>((event, emit) async {
       emit(CodeSent(
           verifyId: event.verificationId, token: event.forceResendingToken));
@@ -86,74 +88,10 @@ class AuthenticationBloc
         emit(response.fold((error) => VerifyOTPFailed(errorMessage: error),
             (response) => VerifyOTPLoaded(credential: response)));
 
-        transformer:
-        restartable();
+        // ignore: unused_label
+        transformer:restartable();
       },
     );
-    // @override
-    // Stream<AuthenticationState> mapEventToState(
-    //   AuthenticationEvent event,
-    // ) async* {
-    //   if (event is SignupLoadingEvent) {
-    //     yield SignupLoading();
-    //   }
-    //   if (event is SignupEvent) {
-    //     //firebaseRun(event.users);
-    //   }
-    //   if (event is SignupCompleteEvent) {
-    //     yield const SignupComplete();
-    //   }
-    //   if (event is SignupErrorEvent) {
-    //     yield GenericError(errorMessage: event.error);
-    //   }
-    //   if (event is CodeSentEvent) {
-    //     yield SignupLoaded(
-    //         verificationId: event.verificationId,
-    //         forceResendingToken: event.forceResendingToken,
-    //         reference: event.users);
-    //   }
-    // }
-
-    // void firebaseRun(UserModel user) async {
-    //   add(const SignupLoadingEvent());
-    //   await firebaseAuth.verifyPhoneNumber(
-    //     timeout: const Duration(seconds: 120),
-    //     phoneNumber: user.phoneNumber,
-    //     verificationCompleted: (PhoneAuthCredential authCredential) async {
-    //       //print("verification completed ${authCredential.smsCode}");
-    //       // print(" ${authCredential.verificationId}");
-    //       User? user = FirebaseAuth.instance.currentUser;
-
-    //       if (authCredential.smsCode != null) {
-    //         try {
-    //           UserCredential credential =
-    //               await user!.linkWithCredential(authCredential);
-    //         } on FirebaseAuthException catch (e) {
-    //           if (e.code == 'provider-already-linked') {
-    //             await firebaseAuth.signInWithCredential(authCredential);
-    //           }
-    //         }
-    //       }
-    //       add(const SignupCompleteEvent());
-    //     },
-    //     verificationFailed: (FirebaseAuthException exception) {
-    //       if (exception.code == 'invalid-phone-number') {
-    //         //  emit(const GenericError(errorMessage: 'invalid-phone-number'));
-    //       }
-    //       add(SignupErrorEvent(error: exception.code));
-    //       //print(exception.code);
-    //     },
-    //     codeSent: (String verificationId, int? forceResendingToken) {
-    //       add(
-    //         CodeSentEvent(
-    //             forceResendingToken: forceResendingToken!,
-    //             verificationId: verificationId,
-    //             users: user),
-    //       );
-    //     },
-    //     codeAutoRetrievalTimeout: (String timeout) {
-    //       // return null;
-    //     },
-    //);
+   
   }
 }
